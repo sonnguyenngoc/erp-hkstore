3035b1e164162aa54407801ca651a846b6486ced
Nguyen Ngoc Son <sonnn@hoangkhang.com.vn>
2018-06-27 13:51:06 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Erp::Hkstore::Engine.routes.draw do
   root to: "frontend/home#index"
   
-  get "chuyen-muc-san-pham.html" => "frontend/category#index", as: :category
+  get "chuyen-muc/may-vi-tinh.html" => "frontend/category#listing", as: :category_listing
+  get "chuyen-muc-san-pham/may-vi-tinh/laptop-asus.html" => "frontend/category#detail", as: :category_detail
+  get "san-pham/laptop-asus/chi-tiet-san-pham.html" => "frontend/category#product_detail", as: :product_detail
   
   get "ve-hoang-khang-incotech.html" => "frontend/information#about_us", as: :about_us
   get "lien-he-voi-hoang-khang-incotech.html" => "frontend/information#contact_us", as: :contact_us
