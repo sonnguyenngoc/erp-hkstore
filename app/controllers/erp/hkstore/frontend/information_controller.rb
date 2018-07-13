3094221c37012ac39cb9beb0f7e5737403af799d
Nguyen Ngoc Son <sonnn@hoangkhang.com.vn>
2018-07-13 08:54:02 +0700

    update

--- app/controllers/erp/hkstore/frontend/information_controller.rb
+++ app/controllers/erp/hkstore/frontend/information_controller.rb
@@ -3,19 +3,15 @@ module Erp
     module Frontend
       class InformationController < Erp::Frontend::FrontendController      
         def about_us
-          @body_class = "template-page"
         end
         
         def contact_us
-          @body_class = "template-page"
         end
         
         def capacity_profile
-          @body_class = "template-page"
         end
         
         def faqs
-          @body_class = "template-page"
         end
       end
     end
